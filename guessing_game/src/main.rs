1133384c4048d99639a0f80482beb1df78ea92db
maciejstanek <eszpag@gmail.com>
2023-11-24 20:29:04 +0100

    [0200] Made the game functionally complete by adding a loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,17 +7,23 @@ fn main() {
     println!("Guess the number!");
     let secret = rand::thread_rng().gen_range(1..=100);
     println!("DBG: secret={}", secret);
-    print!("Input your guess: ");
-    io::stdout().flush().unwrap(); // unwrap discards any errors
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read!");
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    println!("You guessed {}", guess);
-    match guess.cmp(&secret) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("Correct!"),
+    loop {
+        print!("Input your guess: ");
+        io::stdout().flush().unwrap(); // unwrap discards any errors
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read!");
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
+        println!("You guessed {}", guess);
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("Correct!");
+                break;
+            }
+        }
     }
 }
