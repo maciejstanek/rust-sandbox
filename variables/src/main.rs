3b658a458fd8b3aa2f74dd73ba750f0eb1c7e57e
maciejstanek <eszpag@gmail.com>
2023-11-26 18:31:51 +0100

    [0301] Added shadowing example

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,15 @@ fn main() {
     println!("x={}", x);
     x = 6;
     println!("x={}", x);
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("THREE_HOURS_IN_SECONDS={}", THREE_HOURS_IN_SECONDS);
+
+    let a = 5;
+    let a = a + 1;
+    println!("outer before a={}", a);
+    {
+        let a = a * 2;
+        println!("inner a={}", a);
+    }
+    println!("outer after a={}", a);
 }
