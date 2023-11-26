8ef3aca9f266bb4d35653f8ea7a6ee38a4f9c994
maciejstanek <eszpag@gmail.com>
2023-11-26 20:20:37 +0100

    [0303] Added return values examples

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -5,4 +5,20 @@ fn main() {
 
 fn another_fun(x: i32, y: char) {
     println!("another func ({} {})", x, y);
+
+    let z = {
+        let x = 12;
+        x + 2
+    };
+    println!("z={}", z);
+    let a = plus_one(five());
+    println!("a={}", a);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
