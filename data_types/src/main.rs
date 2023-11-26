77f55100f1fb4f606383f11e4bf31ec0b546496b
maciejstanek <eszpag@gmail.com>
2023-11-26 19:21:10 +0100

    [0302] Added type annotation and value notation examples

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let x1: i32 = -98_222;
+    println!("x1={}", x1);
+    let x2: u8 = 0xff;
+    println!("x2={}", x2);
+    let x3: u16 = 0o77;
+    println!("x3={}", x3);
+    let x4: u8 = 0b1111_0000;
+    println!("x4={}", x4);
+    let x5: isize = 98_222;
+    println!("x5={}", x5);
+    let x6: u8 = b'A';
+    println!("x6={}", x6);
 }
