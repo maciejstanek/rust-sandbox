8538c4778ff338fa453ce1f453dece5d20162c9c
maciejstanek <eszpag@gmail.com>
2023-11-26 19:51:33 +0100

    [0302] Added out of bounds array access example

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     let x1: i32 = -98_222;
     println!("x1={}", x1);
@@ -51,4 +53,23 @@ fn main() {
     println!("may={:?}", may);
     let ar = [3; 5];
     println!("ar={:?}", ar);
+
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
