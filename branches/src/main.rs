d739c466061509f4db13c53df547a353cc4eb40f
maciejstanek <eszpag@gmail.com>
2023-12-03 22:47:36 +0100

    [0305] Added 'if else' example

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -5,4 +5,19 @@ fn main() {
     } else {
         println!("condition was false");
     }
+    if number != 0 {
+        println!("the number was not a zero");
+    }
+
+    let number = 17;
+    println!("{} is...", number);
+    if number % 4 == 0 {
+        println!("divisible by 4");
+    } else if number % 3 == 0 {
+        println!("divisible by 3");
+    } else if number % 2 == 0 {
+        println!("divisible by 2");
+    } else {
+        println!("not divisible by either 4, 3 or 2");
+    }
 }
