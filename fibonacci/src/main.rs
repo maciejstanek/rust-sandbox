58d977976340b040ea3674217b1e7ad07081f021
maciejstanek <eszpag@gmail.com>
2023-12-06 22:27:56 +0100

    [0305] Added a memoized Fibonacci implementation

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -8,9 +8,28 @@ fn fib(x: u64) -> u64 {
     }
 }
 
+fn fib2(x: u64) -> u64 {
+    let mut memo: Vec<u64> = Vec::new();
+    memo.push(0);
+    if x > 0 {
+        memo.push(1);
+    }
+    let mut i = 2;
+    while i <= x {
+        memo.push(memo.iter().rev().take(2).sum());
+        i += 1;
+    }
+    *memo.last().unwrap()
+}
+
 fn main() {
     let mut x = String::new();
     io::stdin().read_line(&mut x).unwrap();
     let x: u64 = x.trim().parse().unwrap();
-    println!("{}", fib(x));
+    if x <= 40 {
+        println!("naive -> {}", fib(x));
+    } else {
+        println!("naive -> too large");
+    }
+    println!("memoized -> {}", fib2(x));
 }
