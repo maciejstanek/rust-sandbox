96e8d5982b776b247680bcaec094730942764635
maciejstanek <eszpag@gmail.com>
2023-12-10 16:40:58 +0100

    Removed comments

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,24 +1,14 @@
 fn main() {
-    let s = String::from("hello"); // s comes into scope
-
-    takes_ownership(s); // s's value moves into the function...
-                        // ... and so is no longer valid here
-
-    let x = 5; // x comes into scope
-
-    makes_copy(x); // x would move into the function,
-                   // but i32 is Copy, so it's okay to still
-                   // use x afterward
-
-    // println!("{}", x);
-    // println!("{}", s);
+    let s = String::from("hello");
+    takes_ownership(s);
+    let x = 5;
+    makes_copy(x);
 
     let mut s1 = String::from("hello");
     change(&mut s1);
     let len = calculate_length(&s1);
     println!("The length of '{}' is {}.", s1, len);
-} // Here, x goes out of scope, then s. But because s's value was moved, nothing
-  // special happens.
+}
 
 fn calculate_length(s: &String) -> usize {
     s.len()
@@ -29,12 +19,9 @@ fn change(some_string: &mut String) {
 }
 
 fn takes_ownership(some_string: String) {
-    // some_string comes into scope
     println!("{}", some_string);
-} // Here, some_string goes out of scope and `drop` is called. The backing
-  // memory is freed.
+}
 
 fn makes_copy(some_integer: i32) {
-    // some_integer comes into scope
     println!("{}", some_integer);
-} // Here, some_integer goes out of scope. Nothing special happens.
+}
