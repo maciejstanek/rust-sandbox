4980fc4b8e017ff21cff3934db2efd306bd94d08
maciejstanek <eszpag@gmail.com>
2023-12-17 21:50:25 +0100

    [0503] Added a method with more parameters than self

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -8,6 +8,9 @@ impl Rect {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+    fn can_hold(&self, other: &Self) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
 }
 
 fn main() {
@@ -20,6 +23,21 @@ fn main() {
     dbg!(&rect);
     println!("area of {:?} is {}", &rect, calculate_area(&rect));
     println!("area of {:?} is {}", &rect, rect.area());
+    let smaller = Rect {
+        width: 2,
+        height: 2,
+    };
+    let larger = Rect {
+        width: 12,
+        height: 13,
+    };
+    let longer = Rect {
+        width: 20,
+        height: 1,
+    };
+    println!("Can hold the smaller rect?: {}", rect.can_hold(&smaller));
+    println!("Can hold the larger rect?: {}", rect.can_hold(&larger));
+    println!("Can hold the longer rect?: {}", rect.can_hold(&longer));
 }
 
 fn calculate_area(rect: &Rect) -> u32 {
