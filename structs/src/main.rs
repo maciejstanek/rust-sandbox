988a0d78d0e9f9d7876384d2fc9265f63814e77c
maciejstanek <eszpag@gmail.com>
2023-12-17 20:11:01 +0100

    [0501] Added examples of struct update syntax

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -14,6 +14,13 @@ fn build_user(email: String, username: String) -> User {
     }
 }
 
+fn print_user(user: User) {
+    println!(
+        "User {{\n  active: {}\n  email: {}\n  username: {}\n  sign_in_count: {}\n}}",
+        user.active, user.username, user.email, user.sign_in_count
+    );
+}
+
 fn main() {
     let mut user1 = User {
         active: true,
@@ -22,5 +29,12 @@ fn main() {
         sign_in_count: 1,
     };
     user1.email = String::from("something@else.com");
+    print_user(user1);
     let user2 = build_user("aaa@bbb.ccc".to_string(), "ddd".to_string());
+    // print_user(user2); // Cannot compile because stuff from user2 was move to user3
+    let user3 = User {
+        email: "xxx@yyy.zzz".to_string(),
+        ..user2
+    };
+    print_user(user3);
 }
