6fb48d2281669c24d3189e33309bd44654338c70
maciejstanek <eszpag@gmail.com>
2023-12-17 20:03:03 +0100

    [0501] Added structs examples

---
+++ structs/src/main.rs
@@ -0,0 +1,26 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username: username,
+        email: email,
+        sign_in_count: 1,
+    }
+}
+
+fn main() {
+    let mut user1 = User {
+        active: true,
+        username: String::from("XxX_user_XxX"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+    user1.email = String::from("something@else.com");
+    let user2 = build_user("aaa@bbb.ccc".to_string(), "ddd".to_string());
+}
